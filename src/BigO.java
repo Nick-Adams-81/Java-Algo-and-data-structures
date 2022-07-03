e8faaa2ee4009157f63d29352edce65ecc38ea55
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-03 18:44:49 -0500

    added O(log n)

--- src/BigO.java
+++ src/BigO.java
@@ -30,8 +30,10 @@ public class BigO {
     }
 
     // Big o = O(1)
-    // explanation: there is only one operation in this method, when there is only one operation the big o value is O(1)
+    // explanation: there is only one operation in this method, when there is only one operation the big o value is O(1), also known as linear time
     public static int addStuff(int n) {
         return n + n;
     }
+
+    // for O(log n) we will imagine we have an array of[1, 2, 3, 4, 5, 6, 7, 8], how many times would we cut the array in half to find the item 1 in this array? we would cut the array in half, then cut it in half again, and again, until we find the item we are looking for in the array. in this case it would be 3 times(or steps), so the logarithm would be log sub2 8 = 3, in big o this is O(log n)
 }
