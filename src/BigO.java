7f32b396aa7c7e9eb84eece4440137dbae53ebae
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-03 17:40:09 -0500

    added O(n)

--- src/BigO.java
+++ src/BigO.java
@@ -1,2 +1,10 @@
 public class BigO {
+
+    // Big o = O(n)
+    // reason this is O(n) is because in this case we run the same number of operations as we pass in, in this case 10
+    public static void printItems(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
     // main method
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        BigO.printItems(10);
     }
 }
