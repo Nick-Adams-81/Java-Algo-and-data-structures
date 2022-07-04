8ff482e8a3156afa6ab3a2dfc037a2bd517b6edb
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-03 19:15:11 -0500

    added different terms for different inputs

--- src/BigO.java
+++ src/BigO.java
@@ -37,7 +37,7 @@ public class BigO {
 
     // for O(log n) we will imagine we have an array of[1, 2, 3, 4, 5, 6, 7, 8], how many times would we cut the array in half to find the item 1 in this array? we would cut the array in half, then cut it in half again, and again, until we find the item we are looking for in the array. in this case it would be 3 times(or steps), so the logarithm would be log sub2 8 = 3, in big o this is O(log n)
 
-    // changing terms for different inputs
+    // different terms for different inputs
     public static void printStuff(int a, int b) {
         for(int i = 0; i < a; i++) {
             System.out.println(i); // O(n) reassigning this value to O(a)
