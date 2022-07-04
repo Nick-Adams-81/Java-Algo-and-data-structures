ab2408f53ac96d63bb6334992093a71aa526e3f0
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-04 12:30:57 -0500

    added cookie class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 
 public class Main {
 
+
     // main method
     public static void main(String[] args) {
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
         BigO.printItems(10);
         BigO.printMoreItems(5);
         System.out.println(BigO.addStuff(3));
         BigO.printStuff(4, 7);
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
     }
 }
