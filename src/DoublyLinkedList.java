85ccf2eb40a253b313bcb9e5e31fa5d7693ecfb6
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-08 14:16:43 -0500

    added remove method

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -2,5 +2,63 @@
 
 public class DoublyLinkedList {
 
+    Node head;
+    Node tail;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getlength() {
+        System.out.println("Length: " + length);
+    }
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if(length == 0) return null;
+        Node temp = tail;
+        if(length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,44 @@ public class Main {
     public static void main(String[] args) {
 
 
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+
+        myDLL.append(2);
+        myDLL.print();
+        myDLL.getlength();
+        myDLL.removeLast();
+        myDLL.print();
 
 
 
 
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.printList();
-        myLinkedList.printLength();
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.printList();
-        myLinkedList.printLength();
-        myLinkedList.removeLast();
-        myLinkedList.printList();
-        myLinkedList.printLength();
-        myLinkedList.prependList(0);
-        myLinkedList.printList();
-        myLinkedList.printLength();
-        myLinkedList.removeFirst();
-        myLinkedList.printList();
-        myLinkedList.printLength();
-        System.out.println(myLinkedList.get(1).value);
-        myLinkedList.insertNode(1, 100);
-        myLinkedList.insertNode(2, 101);
-        myLinkedList.printList();
-        myLinkedList.set(1, 10);
-        myLinkedList.printList();
-        myLinkedList.remove(2);
-        myLinkedList.printList();
-        myLinkedList.reverseList();
-        myLinkedList.printList();
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.printList();
+//        myLinkedList.printLength();
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.printList();
+//        myLinkedList.printLength();
+//        myLinkedList.removeLast();
+//        myLinkedList.printList();
+//        myLinkedList.printLength();
+//        myLinkedList.prependList(0);
+//        myLinkedList.printList();
+//        myLinkedList.printLength();
+//        myLinkedList.removeFirst();
+//        myLinkedList.printList();
+//        myLinkedList.printLength();
+//        System.out.println(myLinkedList.get(1).value);
+//        myLinkedList.insertNode(1, 100);
+//        myLinkedList.insertNode(2, 101);
+//        myLinkedList.printList();
+//        myLinkedList.set(1, 10);
+//        myLinkedList.printList();
+//        myLinkedList.remove(2);
+//        myLinkedList.printList();
+//        myLinkedList.reverseList();
+//        myLinkedList.printList();
 
 
 
