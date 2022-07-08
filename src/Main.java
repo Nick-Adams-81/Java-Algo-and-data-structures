feb6b24ef5980902a417701cd312da13e7c9cfb0
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-08 12:17:50 -0500

    removed dll methods

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,10 @@ public class Main {
     // main method
     public static void main(String[] args) {
 
-        // pointers
-//        HashMap<String, Integer> map1 = new HashMap<>();
-//        HashMap<String, Integer> map2 = new HashMap<>();
-//        HashMap<String, Integer> map3 = new HashMap<>();
 
-//        map1.put("Value", 11);
-//        map2 = map1; // this is a "pointer" to map1
-//
-//        map1.put("Value", 22);
-//        map3.put("value", 34);
-//        map2 = map3; // this is a "pointer" to map3
-//
-//        System.out.println(map1);
-//        System.out.println(map2);
-//
-//        map1 = map3; // we are "pointing" map1 to map3
-//        System.out.println(map1);
 
-        // linked list
+
+
 
 
         LinkedList myLinkedList = new LinkedList(1);
@@ -37,8 +22,6 @@ public class Main {
         myLinkedList.removeLast();
         myLinkedList.printList();
         myLinkedList.printLength();
-//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-//        myDLL.printList();
         myLinkedList.prependList(0);
         myLinkedList.printList();
         myLinkedList.printLength();
@@ -57,29 +40,7 @@ public class Main {
         myLinkedList.printList();
 
 
-//        myDLL.append(2);
-//        myDLL.printList();
-//        myDLL.getLength();
-//        myDLL.removeLast();
-//        myDLL.printList();
-//        myDLL.prepend(0);
-//        myDLL.printList();
-//        myDLL.removeFirst();
-//        myDLL.printList();
-//        myDLL.prepend(0);
-//        myDLL.append(2);
-//        myDLL.append(3);
-//        myDLL.append(4);
-//        myDLL.printList();
-//
-//
-//        System.out.println(myDLL.get(4).value);
-//        myDLL.set(4, 40);
-//        myDLL.printList();
-//        myDLL.insertNode(5, 5);
-//        myDLL.printList();
-//        myDLL.removeNode(5);
-//        myDLL.printList();
+
 
 
     }
