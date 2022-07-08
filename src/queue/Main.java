5df2589523f79f378a56bc5277adee85a53f34ad
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-08 16:30:05 -0500

    added dequeue method

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Queue myQueue = new Queue(1);
 
+        myQueue.printQueue();
+        myQueue.getLength();
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        myQueue.printQueue();
+        myQueue.getLength();
+        myQueue.dequeue();
+        myQueue.printQueue();
+        myQueue.getLength();
+        myQueue.dequeue();
+        myQueue.printQueue();
+        myQueue.getLength();
+
     }
 }
