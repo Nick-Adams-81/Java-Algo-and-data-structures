b13f639265f2a4e3564a080ea1fff377a983852d
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 15:02:19 -0500

    added stack constructor

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -3,6 +3,10 @@ package stack;
 public class Main {
     public static void main(String[] args) {
 
+        Stack myStack = new Stack(1);
+        myStack.printStack();
+        myStack.printHeight();
+        myStack.printTop();
 
     }
 }
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,5 +2,37 @@ package stack;
 
 public class Stack {
 
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printHeight() {
+        System.out.println("Height: " + height);
+    }
+
+    public void printTop() {
+        System.out.println("Top: " + top.value);
+    }
 
 }
