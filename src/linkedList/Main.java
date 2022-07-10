ea8ee8ae1fa772db2c0feeb4c464f87cb7ca97af
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 12:57:33 -0500

    removed main method calls

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -2,9 +2,6 @@ package linkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.printList();
+
     }
 }
