8e1a40d0bc95b3afbfb46d5c26e6b28a799d17e0
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 14:16:23 -0500

    added constructor

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -2,4 +2,31 @@ package doublyLinkedList;
 
 public class DoublyLinkedList {
 
+    Node head;
+    Node tail;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 0;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
 }
--- src/doublyLinkedList/Main.java
+++ src/doublyLinkedList/Main.java
@@ -3,6 +3,7 @@ package doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
 
-
+        DoublyLinkedList myDll = new DoublyLinkedList(1);
+        myDll.printList();
     }
 }
