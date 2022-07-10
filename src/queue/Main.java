8f3df3b24d6938ddee1b7e4a8679779efbd757ab
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 15:16:01 -0500

    added constructor for queue

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -2,6 +2,10 @@ package queue;
 
 public class Main {
     public static void main(String[] args) {
+        Queue myQueue = new Queue(1);
+        myQueue.printQueue();
+        myQueue.getFirst();
+        myQueue.getLast();
 
 
     }
