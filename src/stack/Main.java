00fb63d89c4545d3ed50350dcfaf8c271725c96c
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 14:54:31 -0500

    removed methods from stack

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -2,15 +2,7 @@ package stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(1);
 
-        myStack.push(2);
-        myStack.push(3);
-        myStack.print();
-        myStack.getHeight();
-        myStack.pop();
-        myStack.print();
-        myStack.getHeight();
 
     }
 }
