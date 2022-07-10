282e6a6e20db3295db2468b67fc737c05e3956c3
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 15:24:05 -0500

    added dequeue method for queue

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -10,6 +10,15 @@ public class Main {
         myQueue.printQueue();
         myQueue.getFirst();
         myQueue.getLast();
+        myQueue.enqueue(3);
+        myQueue.printQueue();
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.dequeue();
+        myQueue.printQueue();
+        myQueue.getFirst();
+        myQueue.getLast();
+
 
 
     }
