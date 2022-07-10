8a8136d0629c9f52f37503c4fb727ec1e7f81520
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 13:51:41 -0500

    removed dll methods

--- src/doublyLinkedList/Main.java
+++ src/doublyLinkedList/Main.java
@@ -3,7 +3,6 @@ package doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        System.out.println(myDLL);
+
     }
 }
