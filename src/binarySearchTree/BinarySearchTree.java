4443b8ae65e61807f89fa8a73f3623d1af25ace2
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-10 21:08:37 -0500

    added binary search tree class

---
+++ src/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package binarySearchTree;
+
+public class BinarySearchTree {
+
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
---
+++ src/binarySearchTree/Main.java
@@ -0,0 +1,7 @@
+package binarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
