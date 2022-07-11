b3ad688577472162e0e1ad31d0370050c59cb490
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-11 17:45:24 -0500

    added insert method bst

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -3,6 +3,14 @@ package binarySearchTree;
 public class Main {
     public static void main(String[] args) {
 
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(100);
+        myBST.insert(50);
+        myBST.insert(150);
+
+        System.out.println(myBST.root.value);
+        System.out.println(myBST.root.left.value);
+        System.out.println(myBST.root.right.value);
 
 
     }
