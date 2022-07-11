f04090fba8477c867ecb8bb0919ec0defe89eda7
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-11 17:48:40 -0500

    added contains method

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -7,10 +7,12 @@ public class Main {
         myBST.insert(100);
         myBST.insert(50);
         myBST.insert(150);
+        myBST.insert(10);
 
         System.out.println(myBST.root.value);
         System.out.println(myBST.root.left.value);
         System.out.println(myBST.root.right.value);
+        System.out.println(myBST.contains(10));
 
 
     }
