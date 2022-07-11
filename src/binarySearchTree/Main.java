c5fb6fc0f33cc64c74ea96e956a20aec62b3f2b8
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-11 13:48:35 -0500

    added insert method

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
         System.out.println(myBST.root);
+        myBST.insert(100);
+        myBST.insert(50);
+        myBST.insert(150);
+        myBST.insert(25);
+        myBST.insert(200);
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println("Right: " + myBST.root.right.value);
+        System.out.println("Left: " + myBST.root.left.value);
+
 
     }
 }
