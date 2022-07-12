b3b6bd409d3cdf0c11c6c43093d49f89e80c25e6
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-12 14:32:49 -0500

    added insert method

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -16,7 +16,29 @@ public class BinarySearchTree {
 
 
     public boolean insert(int value) {
-
+        Node newNode = new Node(value);
+        if(root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(temp != null) {
+            if(newNode.value == temp.value) return false;
+            if(newNode.value < temp.value) {
+                if(temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if(temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+        return false;
     }
 
 
--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         BinarySearchTree myBST = new BinarySearchTree();
-        System.out.println(myBST.root);
+        myBST.insert(50);
+        myBST.insert(100);
+        myBST.insert(25);
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println("Right: " + myBST.root.right.value);
+        System.out.println("Left: " + myBST.root.left.value);
     }
 }
