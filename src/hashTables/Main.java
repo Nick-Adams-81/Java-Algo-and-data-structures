77b13c8e184c55bd51ec60ec69b97c3b27f984b4
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-12 16:15:19 -0500

    removed hash table methods

--- src/hashTables/Main.java
+++ src/hashTables/Main.java
@@ -3,17 +3,7 @@ package hashTables;
 public class Main {
 
     public static void main(String[] args) {
-        HashTable myHashTable = new HashTable();
-        myHashTable.set("nails", 100);
-        myHashTable.set("hammer", 1400);
-        myHashTable.set("lumber", 80);
-        myHashTable.set("tile", 45);
-        myHashTable.set("bolts", 30);
-        myHashTable.set("screws", 50);
-        myHashTable.printTable();
-        System.out.println(myHashTable.get("screws"));
-        System.out.println(myHashTable.get("nails"));
-        System.out.println(myHashTable.keys());
+
 
     }
 
