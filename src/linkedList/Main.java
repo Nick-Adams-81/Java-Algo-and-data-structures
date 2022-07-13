c50d2dc4e2f3a8b9e6974180c330b4d208168ce4
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 16:46:56 -0500

    removed linked list methods

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -4,61 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.append(4);
-        myLinkedList.prepend(0);
-
-
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
-        myLinkedList.removeLast();
-
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
-        myLinkedList.removeFirst();
-
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
-        System.out.println(myLinkedList.get(2).value);
-        myLinkedList.insert(3, 500);
-
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
-        myLinkedList.set(3, 50);
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
-        myLinkedList.remove(0);
-        myLinkedList.printList();
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-        myLinkedList.printList();
-        myLinkedList.reverse();
-        myLinkedList.printList();
-
-
-
-
-
-
-
 
     }
 }
