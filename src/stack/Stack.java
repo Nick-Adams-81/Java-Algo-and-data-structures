9dee04cb449e02e7393b11433aa5c9fea6e0623b
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 15:09:26 -0500

    adde stack constructor

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,5 +2,30 @@ package stack;
 
 public class Stack {
 
+    Node top;
+    int height;
+
+    class Node {
+        Node next;
+        int value;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("");
+    }
 
 }
