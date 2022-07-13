3320ee06b8e23bbe44f8867e5359bbbca148569d
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 15:20:38 -0500

    removed queue methods

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -2,22 +2,7 @@ package queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue myQueue = new Queue(1);
-        myQueue.printQueue();
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.enqueue(2);
-        myQueue.printQueue();
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.enqueue(3);
-        myQueue.printQueue();
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.dequeue();
-        myQueue.printQueue();
-        myQueue.getFirst();
-        myQueue.getLast();
+
 
 
 
