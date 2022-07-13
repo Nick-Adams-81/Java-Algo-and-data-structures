922d1dc5d2977d33c94ec40f2de4e93b65396974
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 15:52:41 -0500

    added contains method

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -3,5 +3,10 @@ package binarySearchTree;
 public class Main {
     public static void main(String[] args) {
 
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(10);
+        myBST.insert(5);
+        myBST.insert(15);
+        System.out.println(myBST.contains(10));
     }
 }
