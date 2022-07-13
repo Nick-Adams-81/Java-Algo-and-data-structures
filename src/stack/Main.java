988eafba52c0dfc23e90660d9fb5d9b2e9b40741
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 15:16:54 -0500

    added pop method

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -3,20 +3,31 @@ package stack;
 public class Main {
     public static void main(String[] args) {
 
-        Stack myStack = new Stack(1);
+        Stack myStack = new Stack(12);
         myStack.printStack();
         myStack.getStackHeight();
         myStack.printTop();
 
-        myStack.push(2);
+        myStack.push(23);
         myStack.printStack();
         myStack.getStackHeight();
         myStack.printTop();
 
-        myStack.push(3);
+        myStack.push(34);
         myStack.printStack();
         myStack.getStackHeight();
         myStack.printTop();
+
+        myStack.pop();
+        myStack.printStack();
+        myStack.getStackHeight();
+        myStack.printTop();
+
+        myStack.pop();
+        myStack.printStack();
+        myStack.getStackHeight();
+        myStack.printTop();
+
 
 
     }
