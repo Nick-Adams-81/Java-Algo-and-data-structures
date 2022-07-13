091fc16d4f932801599e7b41014d22c730572df1
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 13:09:33 -0500

    added append method

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
 
 
 
