3d9eabb80d367d61d2e10e6d6e245288996b920b
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-14 15:47:55 -0500

    added dll constructor

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -2,4 +2,46 @@ package doublyLinkedList;
 
 public class DoublyLinkedList {
 
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("");
+    }
+
+    public void printHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void printTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void printLength() {
+        System.out.println("Length: " + length);
+    }
+
+
 }
