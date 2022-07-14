a3543f45dcd2427e39c357a0f5b0c1e60025dfa5
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 20:29:08 -0500

    added append method

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -2,5 +2,58 @@ package doublyLinkedList;
 
 public class DoublyLinkedList {
 
+    Node head;
+    Node tail;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("");
+    }
+
+    public void printHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void printTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
 
 }
--- src/doublyLinkedList/Main.java
+++ src/doublyLinkedList/Main.java
@@ -3,6 +3,14 @@ package doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.printList();
+
+        myDLL.append(2);
+        myDLL.printHead();
+        myDLL.printTail();
+        myDLL.getLength();
+        myDLL.printList();
 
     }
 }
