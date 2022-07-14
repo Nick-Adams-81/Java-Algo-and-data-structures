e2cc8aab4b5dabdc0c9b0a4199b4408ff77ba11f
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 20:22:06 -0500

    removed dll methods

--- src/doublyLinkedList/Main.java
+++ src/doublyLinkedList/Main.java
@@ -3,29 +3,6 @@ package doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.printList();
-
-        myDLL.append(2);
-        myDLL.printList();
-        myDLL.removeLast();
-        myDLL.printList();
-        myDLL.append(2);
-        myDLL.prepend(0);
-        myDLL.printList();
-        myDLL.removeFirst();
-        myDLL.printList();
-        System.out.println(myDLL.get(1).value);
-        myDLL.set(1, 30);
-        myDLL.printList();
-        myDLL.insert(2, 500);
-        myDLL.printList();
-        myDLL.remove(1);
-        myDLL.printList();
-        myDLL.printLength();
-
-
-
 
     }
 }
