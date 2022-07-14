44feaeef35752fe267ad6b5e4866822993ec6d54
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-13 21:07:08 -0500

    added push method

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -3,6 +3,16 @@ package stack;
 public class Main {
     public static void main(String[] args) {
 
+        Stack myStack = new Stack(1);
+        myStack.printStack();
+        myStack.printTop();
+        myStack.getHeight();
+        myStack.push(2);
+        myStack.push(3);
+        myStack.printStack();
+        myStack.printTop();
+        myStack.getHeight();
+
 
     }
 }
