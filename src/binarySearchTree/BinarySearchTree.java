3d131100d3f4d183801afc09f406c61e4e7df441
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-14 17:44:30 -0500

    added insert method

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -2,5 +2,50 @@ package binarySearchTree;
 
 public class BinarySearchTree {
 
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void printRoot() {
+        System.out.println("Root: " + root.value);
+    }
+
+    public void printLeftChild() {
+        System.out.println("Left child: " + root.left.value);
+    }
+
+    public void printRightChild() {
+        System.out.println("Right child: " + root.right.value);
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) root = newNode;
+        Node temp = root;
+        while(true) {
+            if(newNode.value == temp.value) return false;
+            if(newNode.value < temp.value) {
+                if(temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if(temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+
+    }
 
 }
--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -3,5 +3,12 @@ package binarySearchTree;
 public class Main {
     public static void main(String[] args) {
 
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(100);
+        myBST.insert(90);
+        myBST.insert(110);
+        myBST.printRoot();
+        myBST.printRightChild();
+        myBST.printLeftChild();
     }
 }
