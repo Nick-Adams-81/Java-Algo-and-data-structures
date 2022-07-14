ae9f5f534d4ae5438c985209ac6b96fad06def96
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-14 16:44:32 -0500

    added queue constructor

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -2,5 +2,44 @@ package queue;
 
 public class Queue {
 
+    Node first;
+    Node last;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public  void printQueue() {
+        Node temp = first;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void printLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void printLength() {
+        System.out.println("Length: " + length);
+    }
+
 
 }
