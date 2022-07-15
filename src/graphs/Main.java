66918685ca7ca5e38a1700bda55eb1a0dd7017c1
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 17:54:37 -0500

    added add edge method

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -8,6 +8,10 @@ public class Main {
         myGraph.addVertex("B");
         myGraph.addVertex("C");
         myGraph.printGraph();
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+        myGraph.printGraph();
 
 
     }
