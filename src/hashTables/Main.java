b246e177dbd07e77e9c8e037f088649e289cd689
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 17:34:59 -0500

    added get method

--- src/hashTables/Main.java
+++ src/hashTables/Main.java
@@ -8,7 +8,12 @@ public class Main {
         myHash.printTable();
         myHash.set("hammer", 50);
         myHash.set("nails", 10);
+        myHash.set("screws", 15);
+        myHash.set("washers", 15);
+        myHash.set("lumber", 75);
+        myHash.set("tiles", 120);
         myHash.printTable();
+        System.out.println(myHash.get("tiles"));
 
     }
 
