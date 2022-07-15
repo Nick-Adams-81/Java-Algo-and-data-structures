30f62f910f2588a001eb1892873967dfe51be80b
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 16:31:01 -0500

    added remove edge mehtod

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -10,5 +10,7 @@ public class Main {
         myGraph.printGraph();
         myGraph.addEdge("A", "B");
         myGraph.printGraph();
+        myGraph.removeEdge("A", "B");
+        myGraph.printGraph();
     }
 }
