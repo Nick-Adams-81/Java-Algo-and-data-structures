cbc7a98d0c2e61758d8c71cd7790100f769dc7a6
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 17:51:25 -0500

    added add vertex method

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -3,6 +3,11 @@ package graphs;
 public class Main {
     public static void main(String[] args) {
 
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.printGraph();
 
 
     }
