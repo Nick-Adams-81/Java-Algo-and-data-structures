5bea2f3096fa042cf53edf63f4cd7fe0f4609e5a
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 16:43:02 -0500

    added remove vertex method

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -7,10 +7,17 @@ public class Main {
 
         myGraph.addVertex("A");
         myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
         myGraph.printGraph();
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("C", "D");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("A", "D");
         myGraph.printGraph();
         myGraph.removeEdge("A", "B");
+        myGraph.printGraph();
+        myGraph.removeVertex("A");
         myGraph.printGraph();
     }
 }
