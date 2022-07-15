e1653147163e82f7185c18e0f44d27249690a9f3
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 17:44:16 -0500

    removed graph methods

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -3,21 +3,7 @@ package graphs;
 public class Main {
     public static void main(String[] args) {
 
-        Graph myGraph = new Graph();
 
-        myGraph.addVertex("A");
-        myGraph.addVertex("B");
-        myGraph.addVertex("C");
-        myGraph.addVertex("D");
-        myGraph.printGraph();
-        myGraph.addEdge("A", "B");
-        myGraph.addEdge("C", "D");
-        myGraph.addEdge("A", "C");
-        myGraph.addEdge("A", "D");
-        myGraph.printGraph();
-        myGraph.removeEdge("A", "B");
-        myGraph.printGraph();
-        myGraph.removeVertex("A");
-        myGraph.printGraph();
+
     }
 }
