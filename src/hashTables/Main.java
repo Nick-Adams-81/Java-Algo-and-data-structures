1f23446d70439cd3c588a01ee1b9d924791b03d2
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 16:02:04 -0500

    added graphs package

--- src/hashTables/Main.java
+++ src/hashTables/Main.java
@@ -11,6 +11,12 @@ public class Main {
         myHashTable.set("nails", 15);
         myHashTable.set("washers", 10);
         myHashTable.set("screws", 35);
+        myHashTable.set("lumber", 125);
+        myHashTable.set("mjolnir", 100000);
+        myHashTable.set("storm-breaker", 200000);
+        myHashTable.set("infinity gauntlet", 1000000000);
+        myHashTable.set("hammer", 25);
+        myHashTable.set("screw driver", 25);
         myHashTable.printTable();
         System.out.println(myHashTable.get("hammer"));
 
