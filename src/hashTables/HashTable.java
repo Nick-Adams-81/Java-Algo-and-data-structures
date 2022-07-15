bc1bc5d2f6689aa254acdd0e956e3a30795d1744
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-15 17:23:36 -0500

    added constructor for hash table

--- src/hashTables/HashTable.java
+++ src/hashTables/HashTable.java
@@ -5,6 +5,34 @@ import java.util.ArrayList;
 
 public class HashTable {
 
+    private Node[] dataMap;
+    private int size = 7;
+
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ": ");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println(i + ": { " + temp.key + ": " + temp.value + " }");
+                temp = temp.next;
+            }
+        }
+    }
 
 
 }
