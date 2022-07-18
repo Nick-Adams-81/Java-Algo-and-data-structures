b0ef89756da2eaf9f984a7f8433f7ee44beae7e9
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-18 12:35:29 -0500

    added constructor for linked list

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+       LinkedList myLinkedList = new LinkedList(1);
+       myLinkedList.printList();
+       myLinkedList.printHead();
+       myLinkedList.printTail();
+       myLinkedList.printLength();
+
+
+
     }
 }
