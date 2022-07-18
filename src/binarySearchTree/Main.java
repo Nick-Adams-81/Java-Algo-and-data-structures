1623ddd38497ae0c52236f6d8cf3efb81602c000
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-18 17:49:11 -0500

    removed bst methods

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -3,14 +3,5 @@ package binarySearchTree;
 public class Main {
     public static void main(String[] args) {
 
-        BinarySearchTree myBST = new BinarySearchTree();
-        myBST.insert(100);
-        myBST.insert(90);
-        myBST.insert(110);
-        myBST.printRoot();
-        myBST.printRightChild();
-        myBST.printLeftChild();
-
-        System.out.println(myBST.contains(90));
     }
 }
