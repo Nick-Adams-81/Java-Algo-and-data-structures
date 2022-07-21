7c4de16f827f853a5fd81ac13e423e7026b0f7a7
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-21 13:47:28 -0500

    sort package added

---
+++ src/basicSorts/BubbleSort.java
@@ -0,0 +1,5 @@
+package basicSorts;
+
+public class BubbleSort {
+
+}
---
+++ src/basicSorts/Main.java
@@ -0,0 +1,9 @@
+package basicSorts;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4, 6, 2, 5, 1, 3};
+    }
+}
