9e9e84a04326a6b9b28d1f716e64d6303cc96404
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-21 15:12:03 -0500

    remove sort methods

--- src/basicSorts/Main.java
+++ src/basicSorts/Main.java
@@ -6,16 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myArray = {4, 6, 2, 5, 1, 3};
-        BubbleSort.bubbleSort(myArray);
-        System.out.println(Arrays.toString(myArray));
-
-        int[] myArray2 = {9, 7, 1, 4, 5};
-        SelectionSort.selectionSort(myArray2);
-        System.out.println(Arrays.toString(myArray2));
-
-        int[] myArray3 = {10, 15, 27, 1, 2, 13, 45};
-        InsertionSort.insertionSort(myArray3);
-        System.out.println(Arrays.toString(myArray3));
     }
 }
