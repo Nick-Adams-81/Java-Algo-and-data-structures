57bb9c5df3f89013dfbbad6b587d9798f4be3981
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-22 12:48:01 -0500

    added selection sort

--- src/basicSorts/Main.java
+++ src/basicSorts/Main.java
@@ -10,5 +10,12 @@ public class Main {
         System.out.println(Arrays.toString(myArr));
         BubbleSort.bubbleSort(myArr);
         System.out.println(Arrays.toString(myArr));
+
+        int[] myArr2 = {9, 12, 10, 8, 11, 7};
+        System.out.println(Arrays.toString(myArr2));
+        SelectionSort.selectionSort(myArr2);
+        System.out.println(Arrays.toString(myArr2));
+
+        int[] myArr3 = {17, 13, 18, 16, 14, 15};
     }
 }
