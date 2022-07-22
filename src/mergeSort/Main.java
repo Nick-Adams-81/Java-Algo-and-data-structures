19b70a95d8455108de2a3e96d1f5278b52e7acc0
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-22 15:25:43 -0500

    removed merge sort

--- src/mergeSort/Main.java
+++ src/mergeSort/Main.java
@@ -8,9 +8,7 @@ public class Main {
 
         int[] arr1 = {1, 7, 9, 5, 3, 10, 2, 4, 8, 6};
 
-        System.out.println(Arrays.toString(MergeSort.mergeSort(arr1)));
 
-        System.out.println(Arrays.toString(arr1));
 
 
 
