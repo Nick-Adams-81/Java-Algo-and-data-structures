88c4316cdad25b4b1d43bd83a70ed7c3f4df0d37
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-22 12:30:45 -0500

    removed bubble sort

--- src/basicSorts/Main.java
+++ src/basicSorts/Main.java
@@ -6,19 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myArray = {4, 2, 6, 1, 5, 3};
-        System.out.println(Arrays.toString(myArray));
-        BubbleSort.bubbleSort(myArray);
-        System.out.println(Arrays.toString(myArray));
-
-        int[] myArray2 = {11, 7, 9, 10, 8, 12};
-        System.out.println(Arrays.toString(myArray2));
-        SelectionSort.selectionSort(myArray2);
-        System.out.println(Arrays.toString(myArray2));
-
-        int[] myArray3 = {18, 13, 17, 14, 16, 15};
-        System.out.println(Arrays.toString(myArray3));
-        InsertionSort.insertionSort(myArray3);
-        System.out.println(Arrays.toString(myArray3));
     }
 }
