0c0d0b6ba29c5a4e66b1960cfe0802fe184a3dd3
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-22 15:03:21 -0500

    added bubble sort

--- src/basicSorts/Main.java
+++ src/basicSorts/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        int[] myArr = {5, 2, 6, 1, 3, 4};
+        System.out.println(Arrays.toString(myArr));
+        BubbleSort.bubbleSort(myArr);
+        System.out.println(Arrays.toString(myArr));
     }
 }
