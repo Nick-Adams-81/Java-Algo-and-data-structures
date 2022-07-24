4be69f69b17ada35a9c5603ee61d800e76c8a67f
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-24 16:49:30 -0500

    added pivot method

--- src/quickSort/Main.java
+++ src/quickSort/Main.java
@@ -1,4 +1,10 @@
 package quickSort;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myArr = {7, 2, 4, 1, 3, 6, 5};
+
+    }
 }
--- src/quickSort/QiuckSort.java
+++ src/quickSort/QiuckSort.java
@@ -1,4 +1,22 @@
 package quickSort;
 
 public class QiuckSort {
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+        return swapIndex;
+    }
 }
