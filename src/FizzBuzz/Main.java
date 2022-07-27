900113a9ad03a330ba0a2a17ceca0a48386f375b
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-27 16:00:36 -0500

    added fizz buzz

---
+++ src/FizzBuzz/Main.java
@@ -0,0 +1,13 @@
+package FizzBuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println(FizzBuzz.fizzBuzz(20));
+    }
+}
