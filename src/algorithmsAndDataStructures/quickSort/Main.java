461d569abc5b7d7c99e6a5c8f622d7224695452b
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-27 15:18:19 -0500

    removed tree traversal methods

--- src/algorithmsAndDataStructures/quickSort/Main.java
+++ src/algorithmsAndDataStructures/quickSort/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myArr = {7, 6, 5, 4, 3, 2, 1};
+        int[] myArr = {3, 1, 7, 4, 2, 6, 5};
 
         QuickSort.quickSort(myArr);
         System.out.println(Arrays.toString(myArr));
