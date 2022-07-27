c890e51f8d012473df20a85cab3a0d83ee4e38d0
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-27 15:28:03 -0500

    added BFS method

--- src/algorithmsAndDataStructures/binarySearchTree/BinarySearchTree.java
+++ src/algorithmsAndDataStructures/binarySearchTree/BinarySearchTree.java
@@ -68,6 +68,25 @@ public class BinarySearchTree {
         return false;
     }
 
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+        while(queue.size() > 0) {
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if(currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
+    }
+
+
 
 
 
