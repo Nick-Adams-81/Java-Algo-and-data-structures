19d195e14031a15c0ffee9f2889241a066aad6ca
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 17:54:16 -0500

    removed trialing zeros

--- src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
+++ src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
@@ -2,11 +2,5 @@ package popularInterviewQuestions.integers;
 
 public class FactorialTrailingZeros {
 
-    public int trailingZeros(int n) {
-        int count = 0;
-        for(int i = 5; i <= n; i = i * 5) {
-            count = count + n / i;
-        }
-        return count;
-    }
+
 }
