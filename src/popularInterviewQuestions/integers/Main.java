1a350a96a6427f42141063f5992f9d711d6d9730
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 14:18:57 -0500

    aded reverse number

--- src/popularInterviewQuestions/integers/Main.java
+++ src/popularInterviewQuestions/integers/Main.java
@@ -6,10 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {5, 2, 1, 2, 1};
+        ReverseInteger revInt = new ReverseInteger();
 
 
         SingleNumber singleNum = new SingleNumber();
         System.out.println(singleNum.singleNumber(arr));
         System.out.println(singleNum.advancedSingleNumber(arr));
+        System.out.println(revInt.reverse(1234567899));
     }
 }
---
+++ src/popularInterviewQuestions/integers/ReverseInteger.java
@@ -0,0 +1,20 @@
+package popularInterviewQuestions.integers;
+
+public class ReverseInteger {
+
+    // max signed 32 bit int is: 2147483647
+    // min signed 32 bit int is: -2147483647
+    public int reverse(int x) {
+        int reversed = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        while(x != 0) {
+            int lastDigit = x % 10;
+            if(reversed > max / 10 || (reversed == max / 10 && lastDigit > 7)) return 0;
+            if(reversed < min / 10 || (reversed == min / 10 && lastDigit < -8)) return 0;
+            reversed = reversed * 10 + lastDigit;
+            x = x / 10;
+        }
+        return reversed;
+    }
+}
