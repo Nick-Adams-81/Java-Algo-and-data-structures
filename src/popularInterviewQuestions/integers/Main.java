b60aa7711766855342dc14f3d63e6aefd5e13683
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 17:27:11 -0500

    added single number methods

--- src/popularInterviewQuestions/integers/Main.java
+++ src/popularInterviewQuestions/integers/Main.java
@@ -15,7 +15,7 @@ public class Main {
         SingleNumber singleNum = new SingleNumber();
         System.out.println(singleNum.singleNumber(arr));
         System.out.println(singleNum.advancedSingleNumber(arr));
-        System.out.println(revInt.reverse(1234567899));
+
         System.out.println(numPal.numberPalindrome(12213));
         System.out.println(trailingZeros.trailingZeros(125));
         System.out.println(myPow.myPow(4, 3));
--- src/popularInterviewQuestions/integers/SingleNumber.java
+++ src/popularInterviewQuestions/integers/SingleNumber.java
@@ -1,5 +1,6 @@
 package popularInterviewQuestions.integers;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,8 +8,28 @@ public class SingleNumber {
 
     // brute force method TC: O(n) SC:O(n)
 
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        Set<Integer> numbers = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(numbers.contains(nums[i])) {
+                numbers.remove(nums[i]);
+            } else {
+                numbers.add(nums[i]);
+            }
+        }
+        return numbers.iterator().next();
+    }
 
     // constant space complexity TC: O(n) SC: O(1)
+
+    public int advancedSingleNumber(int[] nums) {
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
 
 
 
