95747610ece4519d0c2a2a5a5482abc2c6d6f4c3
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 13:54:21 -0500

    removed single number methods

--- src/popularInterviewQuestions/singleNumber/Main.java
+++ src/popularInterviewQuestions/singleNumber/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {4, 2, 1, 2, 1};
 
+
         SingleNumber singleNum = new SingleNumber();
         System.out.println(singleNum.singleNumber(arr));
         System.out.println(singleNum.advancedSingleNumber(arr));
--- src/popularInterviewQuestions/singleNumber/SingleNumber.java
+++ src/popularInterviewQuestions/singleNumber/SingleNumber.java
@@ -6,27 +6,10 @@ import java.util.Set;
 public class SingleNumber {
 
     // brute force method TC: O(n) SC:O(n)
-    public int singleNumber(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                set.remove(nums[i]);
-            } else {
-                set.add(nums[i]);
-            }
-        }
-        return set.iterator().next();
-    }
+
 
     // constant space complexity TC: O(n) SC: O(1)
-    public int advancedSingleNumber(int[] nums) {
-        int answer = nums[0];
-        for(int i = 1; i < nums.length; i++) {
-            answer = answer ^ nums[i];
-        }
-        return answer;
-    }
+
 
 
 
