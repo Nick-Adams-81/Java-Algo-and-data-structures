4e121d320588be509c3709197ec8f9277f8548e1
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 17:56:04 -0500

    added trailing zeros

--- src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
+++ src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
@@ -3,4 +3,11 @@ package popularInterviewQuestions.integers;
 public class FactorialTrailingZeros {
 
 
+    public int trailingZeros(int num) {
+        int count = 0;
+        for(int i = 5; i <= num; i = i * 5) {
+            count = count + num / i;
+        }
+        return count;
+    }
 }
