0fba6debfe7f57fe7e760b50133d97ce4a7e2906
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-28 13:59:54 -0500

    added single number functions

--- src/popularInterviewQuestions/singleNumber/Main.java
+++ src/popularInterviewQuestions/singleNumber/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[] arr = {4, 2, 1, 2, 1};
+        int[] arr = {5, 2, 1, 2, 1};
 
 
         SingleNumber singleNum = new SingleNumber();
