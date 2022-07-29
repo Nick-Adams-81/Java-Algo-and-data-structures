85452a6e96132908fddc5d25958980b5bfb41c93
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-29 15:52:38 -0500

    added reverse string method

--- src/popularInterviewQuestions/strings/Main.java
+++ src/popularInterviewQuestions/strings/Main.java
@@ -1,4 +1,14 @@
 package popularInterviewQuestions.strings;
 
 public class Main {
+
+
+
+    public static void main(String[] args) {
+        ReverseString revStr = new ReverseString();
+        char[] myArr = {'h', 'e', 'l', 'l', 'o'};
+
+        revStr.reverseString(myArr);
+        System.out.println(myArr);
+    }
 }
---
+++ src/popularInterviewQuestions/strings/ReverseString.java
@@ -0,0 +1,20 @@
+package popularInterviewQuestions.strings;
+
+import java.util.ArrayList;
+
+public class ReverseString {
+
+    public void reverseString(char[] string) {
+        int left = 0;
+        int right = string.length -1;
+        while(left < right) {
+            char temp = string[left];
+            string[left] = string[right];
+            string[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+
+}
