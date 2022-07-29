ea72fd4b32370a5a80bdf766e930a8c1c31a0f44
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-29 17:13:14 -0500

    added longest prefix method

--- src/popularInterviewQuestions/strings/Main.java
+++ src/popularInterviewQuestions/strings/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         ReverseString revStr = new ReverseString();
+        LongestCommomPrefix myPrefix = new LongestCommomPrefix();
         char[] myArr = {'h', 'e', 'l', 'l', 'o'};
 
+        String[] myStrArr = {"flower", "flow", "float"};
         revStr.reverseString(myArr);
         System.out.println(myArr);
+        System.out.println(myPrefix.longestPrefix(myStrArr));
     }
 }
