29545ac35c8ca69d232501c73daa43bb24a6f406
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-01 11:03:43 -0500

    added palindomic substring method

--- src/popularInterviewQuestions/strings/Main.java
+++ src/popularInterviewQuestions/strings/Main.java
@@ -10,15 +10,19 @@ public class Main {
         RepeatedDNASequence myDNA = new RepeatedDNASequence();
         LongestSubstringWithoutRepeatingChars substringChars = new LongestSubstringWithoutRepeatingChars();
         ValidParentheses isValid = new ValidParentheses();
-        char[] myArr = {'h', 'e', 'l', 'l', 'o'};
+        LongestPalindromicSubstring isPalindrome = new LongestPalindromicSubstring();
 
+        char[] myArr = {'h', 'e', 'l', 'l', 'o'};
         String myString = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
         String[] myStrArr = {"flower", "flow", "float"};
+        String newString = "maddam";
+
         revStr.reverseString(myArr);
         System.out.println(myArr);
         System.out.println(myPrefix.longestPrefix(myStrArr));
         System.out.println(myDNA.findDNASubString(myString));
         System.out.println(substringChars.lengthOfLongestSubstring("abcdeffg"));
         System.out.println(isValid.isValid("[]{}"));
+        System.out.println(isPalindrome.longestPalindrome(newString));
     }
 }
