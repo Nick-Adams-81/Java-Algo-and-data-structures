c9a4afb56afdf8bef8a8adb11376af132ec51885
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-02 15:39:06 -0500

    added rotated sorted array method

--- src/popularInterviewQuestions/binarySearch/Main.java
+++ src/popularInterviewQuestions/binarySearch/Main.java
@@ -1,11 +1,13 @@
-package binarySearch;
+package popularInterviewQuestions.binarySearch;
 
 public class Main {
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
+        RotateSortedArray rotatedArr = new RotateSortedArray();
         int[] myArr = {1, 2, 3, 4, 5, 6, 7};
         System.out.println(binarySearch.search(myArr, 7));
+        System.out.println(rotatedArr.search(myArr, 4));
     }
 
 
