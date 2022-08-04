440e3c8572b92cbb85439b981c9f96a398562405
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-04 11:58:47 -0500

    added min in rotated array

--- src/popularInterviewQuestions/binarySearch/Main.java
+++ src/popularInterviewQuestions/binarySearch/Main.java
@@ -5,9 +5,13 @@ public class Main {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         RotateSortedArray rotatedArr = new RotateSortedArray();
+        MinimumInRotatedSortedArray minRotated = new MinimumInRotatedSortedArray();
+
         int[] myArr = {1, 2, 3, 4, 5, 6, 7};
+        int[] rotatedArray = {3, 4, 5, 1, 2};
         System.out.println(binarySearch.search(myArr, 7));
         System.out.println(rotatedArr.search(myArr, 5));
+        System.out.println(minRotated.findMin(rotatedArray));
     }
 
 
