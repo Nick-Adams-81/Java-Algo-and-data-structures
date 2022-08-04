8c20376c9c0190940849250ddcaad1da16601964
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-04 15:00:48 -0500

    added max profit method

---
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -0,0 +1,11 @@
+package popularInterviewQuestions.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        BuyAndSellStock myStock = new BuyAndSellStock();
+
+        int[] myArr = {7, 1, 5, 3, 6, 4};
+        System.out.println(myStock.maxProfit(myArr));
+
+    }
+}
