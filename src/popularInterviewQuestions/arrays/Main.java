2b536c68d557b973f072d32bab9d01be839e9e44
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-05 13:06:30 -0500

    added two sum method

--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -6,15 +6,18 @@ public class Main {
     public static void main(String[] args) {
         BuyAndSellStock myStock = new BuyAndSellStock();
         MoveZeros moveZeros = new MoveZeros();
+        TwoSum twoSum = new TwoSum();
 
         int[] myArr = {7, 1, 5, 3, 8, 4};
         int[] zerozArr = {1, 0, 4, 23, 0, 8, 12};
         int[] moreZeroz = {0, 4, 8, 0, 1, 2, 0, 9, 12, 0, 7, 13};
+        int[] myTwoSum = {2, 7, 11, 15};
         System.out.println(myStock.maxProfit(myArr));
         moveZeros.moveZeros(zerozArr);
         System.out.println(Arrays.toString(zerozArr));
         moveZeros.optimizedMoveZeros(moreZeroz);
         System.out.println(Arrays.toString(moreZeroz));
+        System.out.println(Arrays.toString(twoSum.twoSum(myTwoSum, 18)));
 
     }
 }
