62ef59944fbabb6507d7659809115c8b494553e1
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-05 14:57:02 -0500

    added uy and sell stock 2 method

--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -10,6 +10,7 @@ public class Main {
         ContainsDuplicate duplicates = new ContainsDuplicate();
         LengthOfLastWord lastWord = new LengthOfLastWord();
         PlaceFlowers flowers = new PlaceFlowers();
+        BuyAndSellStockII moreStock = new BuyAndSellStockII();
 
         int[] myArr = {7, 1, 5, 3, 8, 4};
         int[] zerozArr = {1, 0, 4, 23, 0, 8, 12};
@@ -18,6 +19,7 @@ public class Main {
         int[] dupes = {1, 2, 5, 8, 5};
         int[] flowerBed = {1, 0, 0, 0, 1};
         int[] newFlowerBed = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+        int[] myStockTwo = {7, 1, 5, 3, 6, 4};
         String str = "hello world";
 
         System.out.println(myStock.maxProfit(myArr));
@@ -30,6 +32,7 @@ public class Main {
         System.out.println(lastWord.lengthOfLastWord(str));
         System.out.println(flowers.canPlaceFlowers(flowerBed, 1));
         System.out.println(flowers.optimizedPlaceFlowers(newFlowerBed, 2));
+        System.out.println(moreStock.maxProfit(myStockTwo));
 
     }
 }
