c1586dcfe725b152686baa68e3a9e53e1b50201e
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-08 15:35:35 -0500

    added two sum method

--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -7,16 +7,19 @@ public class Main {
 
         BuyAndSellStock myStock = new BuyAndSellStock();
         MoveZeros moveZeros = new MoveZeros();
+        TwoSum twoSum = new TwoSum();
 
         int[] stock = {7, 1, 5, 3, 6, 4};
         int[] zeros = {0, 3, 5, 0, 12, 0, 4, 6};
         int[] moreZeros = {9, 3, 0, 6, 1, 0, 0, 8, 4};
+        int[] twosum = {2, 7, 11, 13};
 
         System.out.println(myStock.maxProfit(stock));
         moveZeros.moveZeros(zeros);
         System.out.println(Arrays.toString(zeros));
         moveZeros.eficientMoveZeros(moreZeros);
         System.out.println(Arrays.toString(moreZeros));
+        System.out.println(Arrays.toString(twoSum.twoSum(twosum, 18)));
 
     }
 }
--- src/popularInterviewQuestions/arrays/TwoSum.java
+++ src/popularInterviewQuestions/arrays/TwoSum.java
@@ -4,6 +4,19 @@ import java.util.HashMap;
 
 public class TwoSum {
 
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+           int diff = target - nums[i];
+           if(map.containsKey(diff)) {
+               answer[0] = i;
+               answer[1] = map.get(diff);
+           } else {
+               map.put(nums[i], i);
+           }
+        }
 
-
+        return answer;
+    }
 }
