adfd933e2e9064ed74521fcfc9f5fbcf7cf7c138
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-08 15:46:45 -0500

    added contains duplicates

--- src/popularInterviewQuestions/arrays/ContainsDuplicate.java
+++ src/popularInterviewQuestions/arrays/ContainsDuplicate.java
@@ -1,9 +1,22 @@
 package popularInterviewQuestions.arrays;
 
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
 
+    // TC: O(n) SC: O(n)
+    public boolean containsDuplicates(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
 
 }
--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -8,11 +8,13 @@ public class Main {
         BuyAndSellStock myStock = new BuyAndSellStock();
         MoveZeros moveZeros = new MoveZeros();
         TwoSum twoSum = new TwoSum();
+        ContainsDuplicate containsDupes = new ContainsDuplicate();
 
         int[] stock = {7, 1, 5, 3, 6, 4};
         int[] zeros = {0, 3, 5, 0, 12, 0, 4, 6};
         int[] moreZeros = {9, 3, 0, 6, 1, 0, 0, 8, 4};
         int[] twosum = {2, 7, 11, 13};
+        int[] duplicates = {1, 2, 3, 4, 5, 6, 7};
 
         System.out.println(myStock.maxProfit(stock));
         moveZeros.moveZeros(zeros);
@@ -20,6 +22,7 @@ public class Main {
         moveZeros.eficientMoveZeros(moreZeros);
         System.out.println(Arrays.toString(moreZeros));
         System.out.println(Arrays.toString(twoSum.twoSum(twosum, 18)));
+        System.out.println(containsDupes.containsDuplicates(duplicates));
 
     }
 }
