c303e4b5d774cfc002dc40e36380757021a92dfc
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-08 18:30:26 -0500

    added length of last word method

--- src/popularInterviewQuestions/arrays/LengthOfLastWord.java
+++ src/popularInterviewQuestions/arrays/LengthOfLastWord.java
@@ -2,5 +2,16 @@ package popularInterviewQuestions.arrays;
 
 public class LengthOfLastWord {
 
-
+    // TC: O(n) SC: O(1)
+    public int lengthOfLastWord(String s) {
+        int right = s.length() -1;
+        while(right >= 0 && s.charAt(right) == ' ') {
+            right--;
+        }
+        int left = right;
+        while(left >= 0 && s.charAt(left) != ' ') {
+            left--;
+        }
+        return right - left;
+     }
 }
--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -9,12 +9,14 @@ public class Main {
         MoveZeros moveZeros = new MoveZeros();
         TwoSum twoSum = new TwoSum();
         ContainsDuplicate containsDupes = new ContainsDuplicate();
+        LengthOfLastWord lastWord = new LengthOfLastWord();
 
         int[] stock = {7, 1, 5, 3, 6, 4};
         int[] zeros = {0, 3, 5, 0, 12, 0, 4, 6};
         int[] moreZeros = {9, 3, 0, 6, 1, 0, 0, 8, 4};
         int[] twosum = {2, 7, 11, 13};
         int[] duplicates = {1, 2, 3, 4, 5, 6, 7};
+        String str = "length of last word in this sentence";
 
         System.out.println(myStock.maxProfit(stock));
         moveZeros.moveZeros(zeros);
@@ -23,6 +25,7 @@ public class Main {
         System.out.println(Arrays.toString(moreZeros));
         System.out.println(Arrays.toString(twoSum.twoSum(twosum, 18)));
         System.out.println(containsDupes.containsDuplicates(duplicates));
+        System.out.println(lastWord.lengthOfLastWord(str));
 
     }
 }
