40c63c6c53c53de4c17e1160d19018f1ee8733e8
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-08 15:25:03 -0500

    added move zeros methods

--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -6,10 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         BuyAndSellStock myStock = new BuyAndSellStock();
+        MoveZeros moveZeros = new MoveZeros();
 
         int[] stock = {7, 1, 5, 3, 6, 4};
+        int[] zeros = {0, 3, 5, 0, 12, 0, 4, 6};
+        int[] moreZeros = {9, 3, 0, 6, 1, 0, 0, 8, 4};
 
         System.out.println(myStock.maxProfit(stock));
+        moveZeros.moveZeros(zeros);
+        System.out.println(Arrays.toString(zeros));
+        moveZeros.eficientMoveZeros(moreZeros);
+        System.out.println(Arrays.toString(moreZeros));
 
     }
 }
--- src/popularInterviewQuestions/arrays/MoveZeros.java
+++ src/popularInterviewQuestions/arrays/MoveZeros.java
@@ -5,7 +5,40 @@ import java.util.Queue;
 
 public class MoveZeros {
 
+    // TC: O(n) SC: O(n)
+    public void moveZeros(int[] nums) {
+        if(nums.length == 1) return;
 
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                queue.add(nums[i]);
+            }
+        }
 
+        int index = 0;
+        while(queue.size() != 0) {
+            nums[index] = queue.poll();
+            index++;
+        }
+        for(int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    // TC: O(n) SC: O(1)
+    public void eficientMoveZeros(int[] nums) {
+        if(nums.length == 1) return;
+        int left = 0;
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        for(int i = left; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
 
 }
