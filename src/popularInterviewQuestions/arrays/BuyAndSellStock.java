7e17df83d28e339edeae820db9f82812571d65cd
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-08 15:09:31 -0500

    added buy and sell stock 1

--- src/popularInterviewQuestions/arrays/BuyAndSellStock.java
+++ src/popularInterviewQuestions/arrays/BuyAndSellStock.java
@@ -3,4 +3,18 @@ package popularInterviewQuestions.arrays;
 public class BuyAndSellStock {
 
 
+    public int maxProfit(int[] prices) {
+        if(prices.length == 1) return 0;
+        int profit = 0;
+        int left = 0;
+        for(int right = 1; right < prices.length; right++) {
+            if(prices[right] > prices[left]) {
+                profit = Math.max(profit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+        }
+        return profit;
+    }
+
 }
--- src/popularInterviewQuestions/arrays/Main.java
+++ src/popularInterviewQuestions/arrays/Main.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        BuyAndSellStock myStock = new BuyAndSellStock();
+
+        int[] stock = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(myStock.maxProfit(stock));
 
     }
 }
