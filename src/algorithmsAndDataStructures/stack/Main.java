9b0ef815690b5ff82743c49c2bc655169ab0cd4b
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-29 11:50:55 -0700

    added push method

--- src/algorithmsAndDataStructures/stack/Main.java
+++ src/algorithmsAndDataStructures/stack/Main.java
@@ -3,6 +3,16 @@ package algorithmsAndDataStructures.stack;
 public class Main {
     public static void main(String[] args) {
 
+        Stack myStack = new Stack(10);
+        myStack.printStack();
+        myStack.printHeight();
+        myStack.getTop();
+
+        myStack.push(20);
+        myStack.printStack();
+        myStack.printHeight();
+        myStack.getTop();
+
 
     }
 }
