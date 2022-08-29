b4a25c94f1bedd30e20380ed16f1eb1b96e04ab1
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-29 10:27:45 -0700

    added dll constructor

--- src/algorithmsAndDataStructures/doublyLinkedList/DoublyLinkedList.java
+++ src/algorithmsAndDataStructures/doublyLinkedList/DoublyLinkedList.java
@@ -2,6 +2,44 @@ package algorithmsAndDataStructures.doublyLinkedList;
 
 public class DoublyLinkedList {
 
+    private Node head;
+    private Node tail;
+    private int length;
 
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printHead() {
+        System.out.println(head.value);
+    }
+
+    public void printTail() {
+        System.out.println(tail.value);
+    }
+
+    public void printLength() {
+        System.out.println(length);
+    }
 
 }
--- src/algorithmsAndDataStructures/doublyLinkedList/Main.java
+++ src/algorithmsAndDataStructures/doublyLinkedList/Main.java
@@ -3,6 +3,11 @@ package algorithmsAndDataStructures.doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.printList();
+        myDLL.printHead();
+        myDLL.printTail();
+        myDLL.printLength();
 
     }
 }
