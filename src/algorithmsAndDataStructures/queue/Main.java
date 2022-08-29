5277a7c4cc822e1a5cce2a2c91a3942912e65d34
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-29 12:00:37 -0700

    queue constructor

--- src/algorithmsAndDataStructures/queue/Main.java
+++ src/algorithmsAndDataStructures/queue/Main.java
@@ -3,5 +3,10 @@ package algorithmsAndDataStructures.queue;
 public class Main {
     public static void main(String[] args) {
 
+        Queue myQueue = new Queue(10);
+        myQueue.printQueue();
+        myQueue.printFirst();
+        myQueue.printLast();
+        myQueue.printLength();
     }
 }
--- src/algorithmsAndDataStructures/queue/Queue.java
+++ src/algorithmsAndDataStructures/queue/Queue.java
@@ -3,4 +3,44 @@ package algorithmsAndDataStructures.queue;
 public class Queue {
 
 
+    private Node first;
+    private Node last;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void printLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void printLength() {
+        System.out.println("Length: " + length);
+    }
+
 }
