3c76f0ef982ac094e7dfd1676ba25788e16c6605
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-29 11:47:43 -0700

    stack constructor

--- src/algorithmsAndDataStructures/stack/Stack.java
+++ src/algorithmsAndDataStructures/stack/Stack.java
@@ -2,4 +2,38 @@ package algorithmsAndDataStructures.stack;
 
 public class Stack {
 
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void printHeight() {
+        System.out.println("Height: " + height);
+    }
+
 }
