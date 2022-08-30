b6b6c5ecf271e4ca3a2646c8d5646e6790ecb6bf
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-30 11:19:23 -0700

    added set method

--- src/algorithmsAndDataStructures/hashTables/Main.java
+++ src/algorithmsAndDataStructures/hashTables/Main.java
@@ -7,7 +7,10 @@ public class Main {
         HashTable myHashTable = new HashTable();
 
         myHashTable.printTable();
-
+        myHashTable.set("Hammer", 10);
+        myHashTable.set("Nails", 5);
+        myHashTable.set("Boards", 25);
+        myHashTable.printTable();
     }
 
 
