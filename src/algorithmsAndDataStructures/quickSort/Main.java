bef2934ce8820a528ae7c59a2810479052bd904a
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-31 19:39:41 -0700

    removed quick sort methods

--- src/algorithmsAndDataStructures/quickSort/Main.java
+++ src/algorithmsAndDataStructures/quickSort/Main.java
@@ -6,10 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myArr = {3, 1, 7, 4, 2, 6, 5};
-
-        QuickSort.quickSort(myArr);
-        System.out.println(Arrays.toString(myArr));
 
     }
 }
