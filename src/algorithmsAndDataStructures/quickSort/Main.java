e68e0e5428f6ef6773918af5ebf05d2617f44382
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-31 19:54:27 -0700

    added swap and pivot methods for quick sort

--- src/algorithmsAndDataStructures/quickSort/Main.java
+++ src/algorithmsAndDataStructures/quickSort/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] myArr = {5, 1, 6, 4, 7, 3, 2};
+
 
     }
 }
--- src/algorithmsAndDataStructures/quickSort/QuickSort.java
+++ src/algorithmsAndDataStructures/quickSort/QuickSort.java
@@ -3,5 +3,22 @@ package algorithmsAndDataStructures.quickSort;
 public class QuickSort {
 
 
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for(int i = pivotIndex + 1; i <= endIndex; i++) {
+            if(array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+        return swapIndex;
+    }
 
 }
