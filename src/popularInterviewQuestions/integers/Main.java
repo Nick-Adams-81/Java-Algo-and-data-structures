06d4a04b998dab7a780723bdb42c6fbc4bd4b7a8
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-01 13:26:56 -0700

    added fizz buzz

--- src/popularInterviewQuestions/integers/Main.java
+++ src/popularInterviewQuestions/integers/Main.java
@@ -14,13 +14,13 @@ public class Main {
 
         System.out.println(FizzBuzz.fizzBuzz(50));
 
-        System.out.println(singleNum.singleNumber(arr));
-        System.out.println(singleNum.advancedSingleNum(arr));
-        System.out.println(revInt.reverseInteger(12345));
-        System.out.println(numPal.numberPalindrome(122));
-        System.out.println(trailingZeros.trailingZeros(25));
-        System.out.println(myPow.myPow(2, 4));
-        System.out.println(NthRootOfNum.root(16.0, 4));
+//        System.out.println(singleNum.singleNumber(arr));
+//        System.out.println(singleNum.advancedSingleNum(arr));
+//        System.out.println(revInt.reverseInteger(12345));
+//        System.out.println(numPal.numberPalindrome(122));
+//        System.out.println(trailingZeros.trailingZeros(25));
+//        System.out.println(myPow.myPow(2, 4));
+//        System.out.println(NthRootOfNum.root(16.0, 4));
 
 
     }
