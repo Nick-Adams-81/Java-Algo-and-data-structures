952979b2afc4994b7e3264fce1fb44a3ff6e0f87
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-31 19:21:48 -0700

    removed merge sort methods

--- src/algorithmsAndDataStructures/mergeSort/Main.java
+++ src/algorithmsAndDataStructures/mergeSort/Main.java
@@ -6,14 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] arr1 = {1, 7, 9, 5, 3, 10, 2, 4, 8, 6};
-        int[] sortedArr1 = {2, 4, 6, 8, 9, 11, 12};
-        int[] sortedArr2 = {1, 3, 5, 7, 10};
-
-
-        System.out.println(Arrays.toString(MergeSort.mergeSort(arr1)));
-        System.out.println(Arrays.toString(arr1));
-
 
     }
 }
