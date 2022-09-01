c0f469958b95dffb1080f476eb98739cdd4d6b42
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-31 19:31:28 -0700

    added merge method

--- src/algorithmsAndDataStructures/mergeSort/Main.java
+++ src/algorithmsAndDataStructures/mergeSort/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] myArr = {4, 7, 1, 3, 6, 2, 5};
+        int[] myArr2 = {1, 2, 3, 4};
+        int[] myArr3 = {5, 6, 7, 8};
+        System.out.println(Arrays.toString(MergeSort.merge(myArr2, myArr3)));
+
 
     }
 }
