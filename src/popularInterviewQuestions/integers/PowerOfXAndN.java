577ee6e21fa2e8a97a3b9ad9e3030be1b48e5df0
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-02 14:00:38 -0700

    removed reverse string method

--- src/popularInterviewQuestions/integers/PowerOfXAndN.java
+++ src/popularInterviewQuestions/integers/PowerOfXAndN.java
@@ -2,4 +2,24 @@ package popularInterviewQuestions.integers;
 
 public class PowerOfXAndN {
 
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long num = n;
+        if (n < 0) {
+            num = -1 * num;
+        }
+        while (num > 0) {
+            if (num % 2 == 0) {
+                x = x * x;
+                num = num / 2;
+            } else {
+                ans = ans * x;
+                num = num - 1;
+            }
+        }
+        if (n < 0) {
+            return 1.0 / ans;
+        }
+        return ans;
+    }
 }
--- src/popularInterviewQuestions/strings/ReverseString.java
+++ src/popularInterviewQuestions/strings/ReverseString.java
@@ -3,17 +3,6 @@ package popularInterviewQuestions.strings;
 
 public class ReverseString {
 
-    public void reverseString(char[] string) {
-        int left = 0;
-        int right = string.length -1;
-        while(left < right) {
-            char temp = string[left];
-            string[left] = string[right];
-            string[right] = temp;
-            left++;
-            right--;
-        }
-    }
 
 
 }
