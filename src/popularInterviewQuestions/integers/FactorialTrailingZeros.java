b44346ba597cc8b67c71bb8f6dfb7d4a4a30ea54
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-02 12:29:47 -0700

    removed trailing zeros method

--- src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
+++ src/popularInterviewQuestions/integers/FactorialTrailingZeros.java
@@ -4,12 +4,5 @@ public class FactorialTrailingZeros {
 
 
     // TC: O(log n) SC: O(1)
-    public int trailingZeros(int x) {
-        int count = 0;
-        for(int i = 5; i <= x; i = i * 5) {
-            count = count + x / i;
-        }
-        return count;
-    }
 
 }
