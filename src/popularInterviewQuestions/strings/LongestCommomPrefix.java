5c367d7647731a8746089d77709a5276df3b2e0f
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-02 14:06:59 -0700

    removed common prefix method

--- src/popularInterviewQuestions/strings/LongestCommomPrefix.java
+++ src/popularInterviewQuestions/strings/LongestCommomPrefix.java
@@ -2,16 +2,6 @@ package popularInterviewQuestions.strings;
 
 public class LongestCommomPrefix {
 
-    public String longestPrefix(String[] strings) {
-        if(strings.length == 0) return "";
-        String prefix = strings[0];
-        for(int i = 1; i < strings.length; i++) {
-            while(strings[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() -1);
-                if(prefix.isEmpty()) return "";
-            }
-        }
-        return prefix;
 
-     }
+
 }
