bab03e69d1af06d902a7edc9330b154fecd9a9cb
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-04 12:19:26 -0700

    removed old trap rain water method

--- src/popularInterviewQuestions/TwoPointers/TrappingRainWater.java
+++ src/popularInterviewQuestions/TwoPointers/TrappingRainWater.java
@@ -18,28 +18,4 @@ public class TrappingRainWater {
         return water;
     }
 
-//    public int trappingRainWater(int[] height) {
-//        int max = 0, n = height.length;
-//        for(int i = 0; i < n; i++) {
-//            if(height[max] < height[i]) {
-//                max = i;
-//            }
-//        }
-//        int sum = 0;
-//        int leftMax = 0;
-//        for(int i = 0; i < max; i++) {
-//            if(height[leftMax] < height[i]) {
-//                leftMax = i;
-//            }
-//            sum += Math.min(height[leftMax], height[max]) - height[i];
-//        }
-//        int rightMax = n - 1;
-//        for(int i = n - 1; i > max; i--) {
-//            if(height[rightMax] < height[i]) {
-//                rightMax = i;
-//            }
-//            sum += Math.min(height[max], height[rightMax]) - height[i];
-//        }
-//        return sum;
-//    }
 }
