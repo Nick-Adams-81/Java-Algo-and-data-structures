659e485cf7e1c08ca9483e1256ab7baa1733f0e5
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-30 18:00:42 -0700

    added racing sorting algos

--- src/algorithmsAndDataStructures/basicSorts/Main.java
+++ src/algorithmsAndDataStructures/basicSorts/Main.java
@@ -2,24 +2,49 @@ package algorithmsAndDataStructures.basicSorts;
 
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 
-        int[] myArr = {5, 2, 6, 1, 3, 4};
+
+
+        long startTimeMills1 = System.currentTimeMillis();
+        long startTime1 = TimeUnit.MILLISECONDS.toSeconds(startTimeMills1);
+        int[] myArr = new int[100000];
+        for(int i = 0; i < myArr.length; i++) {
+            myArr[i] = (int) (Math.random() * 100000);
+            for(int j = 0; j < i; j++) {
+                if(myArr[i] == myArr[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        long endTimeMills1 = System.currentTimeMillis();
+        long endTime1 = TimeUnit.MILLISECONDS.toSeconds(endTimeMills1);
+        long elapsedTime = (endTime1 - startTime1);
+        System.out.println("the loop took: " + elapsedTime + " seconds");
+
+
+        long startTimeMills = System.currentTimeMillis();
+        long startTime3 = TimeUnit.MILLISECONDS.toSeconds(startTimeMills);
         BubbleSort.bubbleSort(myArr);
-        System.out.println(Arrays.toString(myArr));
+//        SelectionSort.selectionSort(myArr);
+//        InsertionSort.insertionSort(myArr);
+        long endTimeMills = System.currentTimeMillis();
+        long endTime3 = TimeUnit.MILLISECONDS.toSeconds(endTimeMills);
+        long elapsedTime3 = (endTime3 - startTime3);
 
+        System.out.println("Bubble sort took: " + elapsedTime3 + " seconds");
+//        System.out.println("Selection sort took " + convert + " seconds");
+//        System.out.println("insertion sort took: " + elapsedTime3 + " seconds");
 
-        int[] myArr2 = {7, 10, 9, 12, 8, 11};
-        SelectionSort.selectionSort(myArr2);
-        System.out.println(Arrays.toString(myArr2));
-
-
-        int[] myArr3 = {18, 13, 17, 16, 14, 15};
-        InsertionSort.insertionSort(myArr3);
-        System.out.println(Arrays.toString(myArr3));
 
     }
 }
